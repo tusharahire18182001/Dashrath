a939f589ca1e9443c470bb4ea8f0cefa93581b2b
tusharahire18182001 <tusharahire18182001@gmail.com>
2025-09-17 11:22:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Test from "./Componants/Test"
+
+function App() {
+  return (
+    <>
+      <Test />
+    </>
+  )
+}
+
+
+export default App
---
+++ src/Componants/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div>
+            hi  iAm the test file
+        </div>
+    )
+}
+
+export default Test
